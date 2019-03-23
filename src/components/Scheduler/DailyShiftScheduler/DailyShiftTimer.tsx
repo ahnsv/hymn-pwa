a18eef6a60b3e5aba0d37775275061f7d1a32a73
ahnopologetic <ahnsv@bc.edu>
2019-03-23 14:39:49 +0900

    bug: firebase deploy is different from dev

--- src/components/Scheduler/DailyShiftScheduler/DailyShiftTimer.tsx
+++ src/components/Scheduler/DailyShiftScheduler/DailyShiftTimer.tsx
@@ -6,6 +6,7 @@
 
 import React from 'react'
 import { getTodayInfo, TimeLeft, castToRealTime } from '../shared'
+import {differenceInMilliseconds} from 'date-fns'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './css/DailyShiftTimer.css'
 
@@ -52,10 +53,10 @@ export default class DailyShiftTimer extends React.Component<DailyShiftTimerProp
         const [start_hr, start_min] = props.start_time.split(":").map(d => parseInt(d))
         const [num_hr, num_mins] = [Math.floor(props.num_of_hrs), props.num_of_hrs % 1]
         const { year: tday_yr, month: tday_month, date: tday_date, day: tday_day } = getTodayInfo()
-        const start_time = new Date(2019, 2, 20, start_hr, start_min, 0)
+        const start_time = new Date(tday_yr, tday_month, tday_date, start_hr, start_min, 0)
         const end_time = new Date(tday_yr, tday_month, tday_date, start_hr + num_hr, start_min + num_mins, 0)
-        const offset = new Date().getTime() - start_time.getTime()
-        const total = end_time.getTime() - start_time.getTime()
+        const offset = differenceInMilliseconds(new Date().getTime(), start_time.getTime())
+        const total = differenceInMilliseconds(end_time.getTime(), start_time.getTime())
         this.setState({ timeLeft: castToRealTime(end_time, now), percentage: (offset / total) })
     }
     render() {
