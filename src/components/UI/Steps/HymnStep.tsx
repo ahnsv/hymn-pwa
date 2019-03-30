e18cce2dac7398c79efe829c80d3db06eace9393
ahnopologetic <ahnsv@bc.edu>
2019-03-31 01:12:55 +0900

    feat: add react swipe

--- src/components/UI/Steps/HymnStep.tsx
+++ src/components/UI/Steps/HymnStep.tsx
@@ -3,47 +3,48 @@ import { Next, Prev, Submit } from "./HymnStepButtons";
 import "./css/HymnStep.css";
 
 export interface StepProps {
-	isActive?: boolean;
-	displayPrevious?: boolean;
-	displayNext?: boolean;
-	displaySubmit?: boolean;
-	prev?: () => {};
-	next?: () => {};
-	children?: React.ReactNode;
-	component?: React.ComponentClass;
+  isActive?: boolean;
+  displayPrevious?: boolean;
+  displayNext?: boolean;
+  displaySubmit?: boolean;
+  prev?: () => {};
+  next?: () => {};
+  children?: React.ReactNode;
+  component?: React.ComponentClass;
 }
 
 export default class Step extends React.Component<StepProps, {}> {
-	constructor(props: StepProps) {
-		super(props);
-	}
-	render() {
-		if (!this.props.isActive) return null;
-		const {
-			isActive,
-			displayPrevious,
-			displayNext,
-			displaySubmit,
-			prev,
-			next,
-			children,
-			component
-		} = this.props;
-		return (
-			<>
-				{component ? React.createElement(component) : children}
-				<div className="step-nav">
-					<Prev
-						isActive={displayPrevious as boolean}
-						goToPrevStep={prev as () => {}}
-					/>
-					<Next
-						isActive={displayNext as boolean}
-						goToNextStep={next as () => {}}
-					/>
-					<Submit isActive={displaySubmit as boolean} />
-				</div>
-			</>
-		);
-	}
+  constructor(props: StepProps) {
+    super(props);
+  }
+  render() {
+    if (!this.props.isActive) return null;
+    const {
+      isActive,
+      displayPrevious,
+      displayNext,
+      displaySubmit,
+      prev,
+      next,
+      children,
+      component
+	} = this.props;
+    return (
+      <>
+        {component ? React.createElement(component) : children}
+        
+          <div className="step-nav">
+            <Prev
+              isActive={displayPrevious as boolean}
+              goToPrevStep={prev as () => {}}
+            />
+            <Next
+              isActive={displayNext as boolean}
+              goToNextStep={next as () => {}}
+            />
+            <Submit isActive={displaySubmit as boolean} />
+          </div>
+      </>
+    );
+  }
 }
--- src/components/UI/Steps/HymnSteps.tsx
+++ src/components/UI/Steps/HymnSteps.tsx
@@ -1,50 +1,55 @@
 import React, { ReactElement, ReactNode } from "react";
+import Swipe from "react-swipe";
 import "./css/HymnSteps.css";
 
-type StepsProps = {
-	children: ReactNode;
-};
+interface StepsProps {
+  children: ReactNode;
+}
 interface StepsState {
-	currentStep: number;
-	stepsCount: number;
+  currentStep: number;
+  stepsCount: number;
 }
 export default class Steps extends React.Component<StepsProps, StepsState> {
-	constructor(props: StepsProps) {
-		super(props);
-		this.state = {
-			currentStep: 0,
-			stepsCount: (this.props.children as any[]).length - 1
-		};
-	}
-	prev = () => {
-		this.setState({
-			currentStep: this.state.currentStep - 1
-		});
-	};
+  constructor(props: StepsProps) {
+    super(props);
+    this.state = {
+      currentStep: 0,
+      stepsCount: (this.props.children as any[]).length - 1
+    };
+  }
+  prev = () => {
+    this.setState({
+      currentStep: this.state.currentStep - 1
+    });
+  };
 
-	next = () => {
-		this.setState({
-			currentStep: this.state.currentStep + 1
-		});
-	};
+  next = () => {
+    this.setState({
+      currentStep: this.state.currentStep + 1
+    });
+  };
 
-	render() {
-		const stepChildren = React.Children.map(
-			this.props.children,
-			(child, index) => {
-				const { currentStep, stepsCount } = this.state;
-				if (React.isValidElement(child)) {
-					return React.cloneElement(child as React.ReactElement<any>, {
-						isActive: index === currentStep,
-						displayPrevious: currentStep > 0,
-						displayNext: currentStep < stepsCount,
-						displaySubmit: currentStep === stepsCount,
-						prev: () => this.prev(),
-						next: () => this.next()
-					});
-				}
-			}
-		);
-		return <div className="hymn-steps">{stepChildren}</div>;
-	}
+  render() {
+    const stepChildren = React.Children.map(
+      this.props.children,
+      (child, index) => {
+        const { currentStep, stepsCount } = this.state;
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child as React.ReactElement<any>, {
+            isActive: index === currentStep,
+            displayPrevious: currentStep > 0,
+            displayNext: currentStep < stepsCount,
+            displaySubmit: currentStep === stepsCount,
+            prev: () => this.prev(),
+            next: () => this.next()
+          });
+        }
+      }
+    );
+    return (
+      <div className="hymn-steps">
+        {stepChildren}
+      </div>
+    );
+  }
 }
